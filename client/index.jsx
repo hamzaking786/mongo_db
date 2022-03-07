02398fac2fd99749810bd61f92450e8a102e1a14
Hamza Dastgir <m.hamzadastgir@outlook.com>
2022-03-07 16:38:16 +0100

    fixed moviedb film dukker ikke opp..

--- client/index.jsx
+++ client/index.jsx
@@ -42,7 +42,7 @@ function useLoading(loadingFunction) {
 
 }
 
-async function fetchJSON(){
+async function fetchJSON(url){
     const res = await fetch(url)
     if (!res.ok){
         throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
